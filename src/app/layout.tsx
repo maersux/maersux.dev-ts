eaaa32143e772b20882b08209932c2cab3c3d529
maersux <marcel@gertsch.be>
2024-08-06 23:31:38 +0200

    fix: moved favicon, fixed svg description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,9 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: `maersux' website`,
   description: `i&apos;m just this random dev guy. currently working on modchecker.com and susgeebot. twitch viewer doing twitch stuff i guess.`,
+  icons: {
+    icon: 'https://maersux.dev/favicon.ico'
+  },
   authors: {
     name: 'maersux',
     url: 'https://github.com/maersux'
