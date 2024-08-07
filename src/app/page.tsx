600bdbf2248fc4a13c99e02d94467a7b1655cdb3
maersux <marcel@gertsch.be>
2024-08-07 10:30:12 +0200

    style: min-height adjustments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { MainHeading } from '@/components/header';
 
 export default function Home() {
   return (
-    <main className="min-h-screen flex flex-col items-center p-12 md:p-24">
+    <main className="min-h-dvh flex flex-col items-center p-12 md:p-24">
       <div className="w-96 max-w-[90%] flex flex-col gap-8">
         <MainHeading />
         <div className="flex flex-row items-center gap-4">
